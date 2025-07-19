64c1e6f2b783fa5c1d5a4e5e0fce3825d0bf3746
dineshpandeyy <dineshpandey3536@gmail.com>
2025-07-18 21:47:19 -0500

    basic ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "../components/Narbar";
+import { resumes } from "../../constants";
+import ResumeCard from "../components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResuLens" },
+    { name: "description", content: "Welcome to resume reviewer!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your job application progress and Resume Rating</h1>
+          <h2>Review your submissions and get feedback on your resume</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resume-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
